d5b8269f2e730099bcb8e86cfcd1660143f716bf
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-27 19:05:00 -0300

    feat: layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,39 @@
-import React from "react";
 import { Header } from "../Header/Header";
 import { Outlet } from "react-router-dom";
 import styles from "./Layout.module.scss";
 import Footer from "../Footer/Footer";
+import { usePokemonList } from "../../hooks/usePokemonList";
 
 export const Layout = () => {
+  const {
+    pokemons,
+    isLoading,
+    search,
+    setSearch,
+    page,
+    totalPages,
+    setPage,
+    clearSearch,
+    isSearchMode,
+  } = usePokemonList();
+
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <Outlet />
+        <Outlet
+          context={{
+            pokemons,
+            isLoading,
+            isSearchMode,
+            page,
+            totalPages,
+            setPage,
+            search,
+            setSearch,
+            clearSearch,
+          }}
+        />
       </main>
       <Footer />
     </>
