46f10cb32d0639d62de5cda78b25b7a24c651cd5
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-27 13:55:02 -0300

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/_reset.scss";
 import "./styles/main.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout/Layout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
