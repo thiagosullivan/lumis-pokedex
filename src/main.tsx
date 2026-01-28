72c5728920a24edf18f5284a24e73373608051ac
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-28 08:31:57 -0300

    corrections for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/_reset.scss";
 import "./styles/main.scss";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PokemonDetail } from "./components/PokemonDetail/PokemonDetail.tsx";
@@ -22,14 +22,14 @@ const basename = import.meta.env.PROD ? "/lumis-pokedex" : "";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename={basename}>
+      <HashRouter basename={basename}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<App />} />
             <Route path="pokemon/:id" element={<PokemonDetail />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </StrictMode>,
 );
